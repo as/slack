7defff89383dbec3cbe0151a41a2780c68515f83
as <as.utf8@gmail.com>
2018-08-10 01:21:26 -0700

    update request; add debug

--- converse/request.go
+++ converse/request.go
@@ -2,9 +2,13 @@ package converse
 
 import (
 	"fmt"
+	"io"
+	"os"
 
 	"github.com/as/slack"
 )
+
+var debug = os.Getenv("slackdebug") == "on"
 
 type Kind int
 
@@ -22,7 +26,7 @@ var kindTab = map[Kind]string{
 	KindIM:      "im",
 }
 
-func List(c *slack.Client, include Kind, list *[]Channel) (next string, err error) {
+func List(c *slack.Client, include Kind, list *[]Channel, cursor ...string) (next string, err error) {
 	var R struct {
 		Ok      bool      `json:"ok"`
 		Channel []Channel `json:"channels"`
@@ -35,23 +39,34 @@ func List(c *slack.Client, include Kind, list *[]Channel) (next string, err erro
 	if cap(*list) != 0 && cap(*list) < 1000 {
 		lim = cap(*list)
 	}
+	if len(cursor) > 0 {
+		next = cursor[0]
+	}
 
 	r, err := c.Post(c.BaseURL()+"/api/conversations.list", slack.Encode(struct {
-		Token string `url:"token"`
-		Limit string `url:"limit"`
-		Types string `url:"types"`
+		Token  string `url:"token"`
+		Limit  string `url:"limit"`
+		Types  string `url:"types"`
+		Cursor string `url:"cursor"`
 	}{
-		Token: c.Token(),
-		Limit: fmt.Sprint(lim),
-		Types: "public_channel,private_channel",
+		Token:  c.Token(),
+		Limit:  fmt.Sprint(lim),
+		Types:  "public_channel,private_channel",
+		Cursor: next,
 	}))
 	if err != nil {
 		return "", err
 	}
 	defer r.Body.Close()
 
+	if debug {
+		slack.Dump(r)
+	}
 	if err = slack.Decode(r, &R); err != nil {
 		return "", err
+	}
+	if len(R.Channel) == 0 || R.Meta.Next == "" {
+		return "", io.EOF
 	}
 	*list = append(*list, R.Channel...)
 	return R.Meta.Next, err
